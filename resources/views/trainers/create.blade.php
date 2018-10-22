c48b118c6d59330241b3b66a1cbefe6f60bc16fd
Ivan5 <converse_ivan@hotmail.com>
2018-10-22 17:39:23 -0500

    Using blade

--- resources/views/trainers/create.blade.php
+++ resources/views/trainers/create.blade.php
@@ -1,21 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Create Trainer</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-</head>
-<body>
-  <div class="container">
+@extends('layouts.app')
+
+@section('title','Create Trainer')
+
+@section('content')
+
     <div class="form-goup">
       <label for="">Nombre</label>
       <input type="text" class="form-control">
     </div>
     <button type="submit" class="btn btn-primary btn-block mt-3">Guardar</button>
-  </div>
-  
-  
-</body>
-</html>
+    
+@endsection
