97f50204fb7871f38275e3eb044fe098bd31e98b
Ivan5 <converse_ivan@hotmail.com>
2018-10-22 17:28:59 -0500

    Add views

---
+++ resources/views/trainers/create.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Create Trainer</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<body>
+  <div class="container">
+    <div class="form-goup">
+      <label for="">Nombre</label>
+      <input type="text" class="form-control">
+    </div>
+    <button type="submit" class="btn btn-primary btn-block mt-3">Guardar</button>
+  </div>
+  
+  
+</body>
+</html>
