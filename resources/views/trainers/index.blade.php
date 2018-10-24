c035a69131a5a13e0f9bacfc138923154000d5fb
Ivan5 <converse_ivan@hotmail.com>
2018-10-23 22:06:02 -0500

    Refactor trainer index

--- resources/views/trainers/index.blade.php
+++ resources/views/trainers/index.blade.php
@@ -7,20 +7,15 @@
     <div class="row">
     @foreach($trainers as $trainer)
       <div class="col-sm">
-       <div class="card mt-5" style="width: 18rem;">
-          <img class="card-img-top" src="images/{{$trainer->avatar}}" alt="">
+       <div class="card mt-5 text-center" style="width: 18rem;">
+          <img style="height:100px; width:100px; background-color: #efefef;" class="card-img-top rounded-circle mt-2 mx-auto d-block" src="images/{{$trainer->avatar}}" alt="">
           <div class="card-body">
             <h5 class="card-title">{{$trainer->name}}</h5>
             <p class="card-text">{{$trainer->descruption}}</p>
-            <a href="" class="btn btn-primary">More..</a>
+            <a href="" class="btn btn-primary">Show..</a>
           </div>
         </div>
       </div>
       @endforeach
     </div>
-    
-
-  
-  
-
 @endsection
