83ec4112663630172959f33513484ac7685a4630
Ivan5 <converse_ivan@hotmail.com>
2018-10-25 16:10:31 -0500

    Use validation and FormRequest

--- app/Http/Controllers/TrainerController.php
+++ app/Http/Controllers/TrainerController.php
@@ -3,6 +3,7 @@
 namespace LaraDex\Http\Controllers;
 use LaraDex\Trainer;
 use Illuminate\Http\Request;
+use LaraDex\Http\Requests\StoreTrainerRequest;
 
 class TrainerController extends Controller
 {
@@ -33,14 +34,15 @@ class TrainerController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreTrainerRequest $request)
     {
+        $trainer = new Trainer();
+
         if($request->hasFile('avatar')){
             $file = $request->file('avatar');
             $name = time().$file->getClientOriginalName();
             $file->move(public_path().'/images/', $name);
         }
-        $trainer = new Trainer();
         $trainer->name = $request->input('name');
         $trainer->avatar = $name;
         $trainer->descruption = $request->input('descruption');
--- resources/views/trainers/create.blade.php
+++ resources/views/trainers/create.blade.php
@@ -3,6 +3,16 @@
 @section('title','Create Trainer')
 
 @section('content')
+  @if ($errors->any())
+    <div class="alert alert-danger">
+    <ul>
+      @foreach($errors->all() as $error)
+        <li>{{$error}}</li>
+      @endforeach
+    </ul>
+    </div>
+   
+  @endif
   {!! Form::open(['route' => 'trainers.store', 'method' => 'POST', 'files' => true]) !!}
     @include('trainers.form')
     {!! Form::submit('Guardar',['class' => 'btn btn-primary btn-block mt-3']) !!}
